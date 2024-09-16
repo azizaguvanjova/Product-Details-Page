dacad8347b7861f5ee7e7eeb3c74071e854e4c90
azizaguvanjova <aziza.guvanjova13@gmail.com>
2024-09-16 06:04:29 +0300

    uptaded project

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import { BrowserRouter,Route,Routes } from 'react-router-dom'
 
 import StoreFront from './pages/StoreFront'
 import ProductDetails from './pages/ProductDetails'
+import { useState,useEffect } from 'react'
+
 export default function App() {
-  return (
+   const [isClient, setIsClient] = useState(false)
+
+   useEffect(()=>{
+    if (typeof window !== "undefined") {
+      setIsClient(true)
+    }
+   },[])
+  return isClient ? (
 <BrowserRouter>
 <Routes>
   <Route path="/" element={<StoreFront/>}/>
   <Route path='/products/:id' element={<ProductDetails/>}/>
 </Routes>
 </BrowserRouter>
+  ) : (
+    <div>Loading...</div>
   )
 }
